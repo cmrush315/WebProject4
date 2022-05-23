aff1565ee5d47a0800e09feb68b3dc79d629a660
Caleb Rush <cmrush15@gmail.com>
2022-05-23 08:28:31 -0500

    Change Git Repository

---
+++ README.md
@@ -0,0 +1,17 @@
+# Around the U.S
+
+## Description
+
+Around the U.S. is an image gallery highlighting interesting locations around the United States. Contains a modal window which allows the user to change their profile name and title.
+
+### Technologies Used:
+
+HTML
+CSS
+Responsive Design
+Javascript
+Git
+
+### Github Pages Link
+
+https://cmrush315.github.io/cmrush315-se_project_aroundtheus/
---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileEditPopup = document.querySelector(".popup");
+const formEditCloseIcon = document.querySelector(".form__close-icon");
+const profileEditForm = document.querySelector(".form");
+const profileNameElement = document.querySelector(".profile__name");
+const profileJobElement = document.querySelector(".profile__job");
+const profileNameInput = form.querySelector(".form__input_name");
+const profileJobInput = form.querySelector(".form__input_job");
+
+function openPopup() {
+  profileEditPopup.classList.add("popup_is-opened");
+  profileNameInput.value = profileNameElement.textContent;
+  profileJobInput.value = profileJobElement.textContent;
+}
+
+function closePopup() {
+  profileEditPopup.classList.remove("popup_is-opened");
+}
+
+profileEditButton.addEventListener("click", openPopup);
+
+formEditCloseIcon.addEventListener("click", closePopup);
+
+function preventDefaultEvent(event) {
+  event.preventDefault();
+  const nameValue = event.target.name.value;
+  const jobValue = event.target.job.value;
+  profileNameElement.textContent = nameValue;
+  profileJobElement.textContent = jobValue;
+  closePopup();
+}
+profileEditForm.addEventListener("submit", preventDefaultEvent);
