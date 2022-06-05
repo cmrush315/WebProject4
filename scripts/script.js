a4900706fc5901eeb532a4bdc940e32dd300b3ee
Caleb Rush <cmrush15@gmail.com>
2022-06-04 19:01:27 -0500

    Submission 4 Changes

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,8 @@ const formEditCloseIcon = document.querySelector(".form__close-icon");
 const profileEditForm = document.querySelector(".form");
 const profileNameElement = document.querySelector(".profile__name");
 const profileJobElement = document.querySelector(".profile__job");
-const profileNameInput = form.querySelector(".form__input_name");
-const profileJobInput = form.querySelector(".form__input_job");
+const profileNameInput = form.querySelector(".form__input_name_name");
+const profileJobInput = form.querySelector(".form__input_name_job");
 
 function openPopup() {
   profileEditPopup.classList.add("popup_is-opened");
